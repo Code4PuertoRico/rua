3aad5b542717dd87b6a4226f7109720399096a01
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-03-02 14:40:00 -0400

    Add initial Agency model

--- rua/apps/agencies/models.py
+++ rua/apps/agencies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Agency(models.Model):
+	# Direct properties
+    registration = models.PositiveIntegerField(verbose_name=_(u'registration'), unique=True)
+    name = models.CharField(max_length=128, verbose_name=_(u'name'), unique=True)
+    
+    # Physical properties
+    physical_address = models.TextField(verbose_name=_(u'physical address'), blank=True)
+    postal_address = models.TextField(verbose_name=_(u'postal address'), blank=True)
+
+    objects = OmitDisabledManager()
+
+    def __unicode__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.registration,)
+
+    class Meta:
+        verbose_name = _(u'agency')
+        verbose_name_plural = _(u'agencies')
+        ordering = ['name']
