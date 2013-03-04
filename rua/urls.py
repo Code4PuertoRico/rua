5b821006e739652e977d48c5119c1583cb4e7b6a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-03-03 23:49:06 -0400

    Add static file serving during DEBUG

--- rua/urls.py
+++ rua/urls.py
@@ -1,11 +1,20 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
-
+    (r'^grappelli/', include('grappelli.urls')),
     (r'^api/', include('api.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
