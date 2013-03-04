92d55c8a659b21ad692bcd62a5accbb3930f4af9
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-03-03 21:21:13 -0400

    Set the default admin URL as the top project URL too

--- rua/urls.py
+++ rua/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    #url(r'^$', 'rua.views.home', name='home'),
-    # url(r'^rua/', include('rua.foo.urls')),
+    url(r'^', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
 
     (r'^api/', include('api.urls')),
