89a9456ba30a9445c11b57a47197516babe37e3a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-03-04 00:32:46 -0400

    Expose aditional agency info via API

--- rua/apps/api/serializers.py
+++ rua/apps/api/serializers.py
@@ -3,10 +3,36 @@ from django.contrib.auth.models import User, Group
 
 from rest_framework import serializers
 
-from agencies.models import Agency
+from agencies.models import (Agency, AgencyPosition, AgencyDepartment,
+    DepartmentLocation)
+
+
+class DepartmentLocationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DepartmentLocation
+        fields = ('start_date', 'end_date', 'physical_address', 'postal_address', 'latitude', 'longitude')
+
+
+class AgencyPositionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = AgencyPosition
+        fields = ('label', 'start_date', 'end_date')
+
+
+class AgencyDepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    locations = DepartmentLocationSerializer()
+
+    class Meta:
+        model = AgencyDepartment
+        fields = ('label', 'start_date', 'end_date', 'locations')
 
 
 class AgencySerializer(serializers.HyperlinkedModelSerializer):
+    positions = AgencyPositionSerializer()
+    departments = AgencyDepartmentSerializer()
+
     class Meta:
         model = Agency
-        fields = ('registration', 'name',)
+        fields = ('url', 'registration', 'name', 'positions', 'departments')
+
+
